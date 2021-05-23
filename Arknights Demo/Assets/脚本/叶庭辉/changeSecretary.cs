856be49d01bdc683967eadc43d74b0f0ccf519d9
Deniseb <armstrongdeniseb12@gmail.com>
2021-05-23 15:24:01 +0800

    更新了看板娘替换功能v0.01版

--- Arknights Demo/Assets/脚本/叶庭辉/changeSecretary.cs
+++ Arknights Demo/Assets/脚本/叶庭辉/changeSecretary.cs
@@ -18,6 +18,11 @@ public class changeSecretary : MonoBehaviour
 
     private void OnMouseDown()
     {
+        //简易的看板娘切换功能（只能切过去，不能切回来）
         Debug.Log("还在开发中！");
+        SpriteRenderer srSecretary = GameObject.Find("secretary").GetComponent<SpriteRenderer>();
+        Texture2D texture2d = (Texture2D)Resources.Load("立绘_阿米娅_skin1");
+        Sprite sp = Sprite.Create(texture2d, srSecretary.sprite.textureRect, new Vector2(0.5f, 0.5f));
+        srSecretary.sprite = sp;
     }
 }
